9decf5ea1f8822105445d89acc110820a58a21b1
Max Fischer <maxfischer2781@gmail.com>
2017-05-06 22:47:35 +0200

    testing repeated import

--- test_include/functionality.py
+++ test_include/functionality.py
@@ -9,7 +9,7 @@ import include
 
 class TestHook(unittest.TestCase):
     def test_import_file(self):
-        """compat: throw method"""
+        """import file path as module"""
         container = tempfile.NamedTemporaryFile()
         shared_secret = 1337
         container.write(textwrap.dedent("""\
@@ -18,8 +18,12 @@ class TestHook(unittest.TestCase):
         """ % shared_secret).encode('ASCII'))
         container.flush()
         module = include.include_file(container.name)
-        self.assertEqual(module.FileClass.shared_secret, shared_secret)
-        self._test_defined(module.FileClass)
+        class_id = id(module.FileClass)
+        for _ in range(3):
+            self.assertEqual(class_id, id(module.FileClass))
+            self.assertEqual(module.FileClass.shared_secret, shared_secret)
+            self._test_defined(module.FileClass)
+            module = include.include_file(container.name)
 
     def _test_defined(self, obj):
         self.assertEqual(obj, pickle.loads(pickle.dumps(obj)))
