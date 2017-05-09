b8abfbc6f3c9bac5ce0dc618dd1b422c5555229f
Max Fischer <maxfischer2781@gmail.com>
2017-05-09 13:17:11 +0200

    installed hooks are tracked via weak references

--- include/__init__.py
+++ include/__init__.py
@@ -1,14 +1,16 @@
 from __future__ import absolute_import
 import sys
+import weakref
 
-IMPORT_HOOKS = {}
+# weak reference to installed hooks
+_IMPORT_HOOKS = weakref.WeakValueDictionary()
 
 
 def include_file(path):
-    if 'file' not in IMPORT_HOOKS:
+    if 'file' not in _IMPORT_HOOKS:
         from . import files
         files.install()
-    import_hook = IMPORT_HOOKS['file']
+    import_hook = _IMPORT_HOOKS['file']
     module_path = import_hook.path2module(path)
     __import__(module_path)
     return sys.modules[module_path]
--- include/files/__init__.py
+++ include/files/__init__.py
@@ -4,8 +4,8 @@ from __future__ import absolute_import
 def install():
     import sys
     from . import import_hook
-    import include
-    if 'file' in include.IMPORT_HOOKS:
+    from .. import _IMPORT_HOOKS
+    if 'file' in _IMPORT_HOOKS:
         return
-    include.IMPORT_HOOKS['file'] = import_hook.FilePathLoader('file')
-    sys.meta_path.append(include.IMPORT_HOOKS['file'])
+    hook = _IMPORT_HOOKS['file'] = import_hook.FilePathLoader('file')
+    sys.meta_path.append(hook)
