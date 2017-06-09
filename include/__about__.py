a24c8912ea38e51622c22a0a7ef748b2e492a455
Max Fischer <maxfischer2781@gmail.com>
2017-06-09 11:57:01 +0200

    bumped version number

--- include/__about__.py
+++ include/__about__.py
@@ -2,7 +2,7 @@ __title__ = 'include'
 __summary__ = 'Dynamic import from files and other sources'
 __url__ = 'https://github.com/maxfischer2781/chainlet'
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __author__ = 'Max Fischer'
 __email__ = 'maxfischer2781@gmail.com'
 __copyright__ = '2017 %s' % __author__
